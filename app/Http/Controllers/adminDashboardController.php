24c402d2b9c3d1e3461a1a5141f4b4bda187c21f
prantopodder98 <prantopodder111@gmail.com>
2023-10-06 22:15:06 +0600

    function added

--- app/Http/Controllers/adminDashboardController.php
+++ app/Http/Controllers/adminDashboardController.php
@@ -3,11 +3,57 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Url;
 
 class adminDashboardController extends Controller
 {
     public function index()
     {
-        return view('backend.dashboard.dashboard');
+        $urls = Url::all();
+        return view('backend.dashboard.dashboard', ['urls' => $urls]);
     }
+
+
+    public function shorten(Request $request)
+    {
+        // Generate a short URL using a hash of the original URL
+        $originalUrl = $request->input('original_url');
+        $shortUrl = md5($originalUrl); // You can use other hash functions like sha1 or sha256
+
+        // Check if the short URL already exists in the database
+        $existingUrl = Url::where('short_url', $shortUrl)->first();
+        if ($existingUrl) {
+            return view('shortened', ['url' => $existingUrl]);
+        }
+
+        $url = new Url();
+        $url->original_url = $request->input('original_url');
+        $url->short_url = $shortUrl;
+        $url->click_count = 0;
+        $url->save();
+
+        // return view('backend.dashboard.dashboard');
+        return redirect()->route('dashboard');
+
+    }
+
+    public function redirectToOriginal($shortUrl)
+    {
+        // Find the original URL based on the short URL
+        $url = Url::where('short_url', $shortUrl)->first();
+
+        if ($url) {
+            // Increment the click count
+            $url->increment('click_count');
+
+            // Redirect to the original URL
+            return redirect($url->original_url);
+        }
+
+        // Handle URL not found
+        abort(404);
+    }
+
+    // Additional methods as needed
+
 }
--- resources/views/backend/dashboard/dashboard.blade.php
+++ resources/views/backend/dashboard/dashboard.blade.php
@@ -1,46 +1,78 @@
 @extends('backend.master')
 @section('content')
-<div class="card w-100 bg-light-info overflow-hidden shadow-none">
-    <div class="card-body position-relative">
-      <div class="row">
-        <div class="col-sm-7">
-          <div class="d-flex align-items-center mb-7">
-            <div class="rounded-circle overflow-hidden me-6">
-              <img src="{{ asset('asset/backend')}}/images/profile/user-1.jpg" alt="" width="40" height="40">
-            </div>
-            <h5 class="fw-semibold mb-0 fs-5">Welcome back!</h5>
-          </div>
+    <div class="card w-100 bg-light-info overflow-hidden shadow-none">
+        <div class="card-body position-relative">
+            <div class="row">
+                <div class="col-sm-7">
+                    <div class="d-flex align-items-center mb-7">
+                        <div class="rounded-circle overflow-hidden me-6">
+                            <img src="{{ asset('asset/backend') }}/images/profile/user-1.jpg" alt="" width="40"
+                                height="40">
+                        </div>
+                        <h5 class="fw-semibold mb-0 fs-5">Welcome back!</h5>
+                    </div>
 
-        </div>
-        <div class="col-sm-5">
-          <div class="welcome-bg-img mb-n7 text-end">
-            <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/backgrounds/welcome-bg.svg" alt="" class="img-fluid">
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-<div class="card">
-    <div class="card-body bg-light">
-        <div class="row border-bottom">
-          <div class="col-sm-12 col-lg-12">
-            <div class="form-group row align-items-center mb-0">
-              <label
-                for="inputEmail3"
-                class="col-1 text-end control-label col-form-label"
-                >URL</label
-              >
-              <div class="col-11 border-start pt-2 pb-2">
-                <input
-                  type="email"
-                  class="form-control"
-                  id="inputEmail3"
-                  placeholder="http://"
-                />
-              </div>
+                </div>
+                <div class="col-sm-5">
+                    <div class="welcome-bg-img mb-n7 text-end">
+                        <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/backgrounds/welcome-bg.svg"
+                            alt="" class="img-fluid">
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-</div>
+    </div>
+    <div class="card">
+        <div class="card-body bg-light">
+            <div class="row border-bottom">
+                <div class="col-sm-12 col-lg-12">
+                    <div class="form-group row align-items-center mb-0">
+                        {{--  <label for="inputEmail3" class="col-1 text-end control-label col-form-label">URL</label>
+                        <div class="col-11 border-start pt-2 pb-2">
+                            <input type="email" class="form-control" id="inputEmail3" placeholder="http://" />
+                        </div>  --}}
+                        <form method="POST" action="{{ route('shorten') }}">
+                            @csrf
+                            <label for="original_url" class="col-1 text-end control-label col-form-label">Enter a
+                                URL:</label>
+                            <input type="text" class="form-control" name="original_url" id="original_url"
+                                placeholder="http://" required>
+                            {{--  <button type="submit">Shorten</button>  --}}
+                            <button type="submit" class="btn btn-info form-control mt-3">
+                                Shorten
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-body bg-light">
+            <div class="table-responsive">
+                <table id="zero_config"
+                    class="table border table-striped table-bordered text-nowrap">
+                <thead class="bg-primary text-white">
+                <tr>
+                    <th>#</th>
+                    <th>Original URL</th>
+                    <th>Short URL</th>
+                    <th>Clicks</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @php $i=1; @endphp
+                    @foreach ($urls as $url)
+                    <tr>
+                        <td>{{ $i++ }}</td>
+                        <td><a href="{{ $url->original_url }}">{{ $url->original_url }}</a></td>
+                        <td><a href="{{ $url->short_url }}">{{ $url->short_url }}</a></td>
+                        <td>{{ $url->click_count }}</td>
+                    </tr>
+                    @endforeach
+            </tbody>
+            </table>
+            </div>
+        </div>
+    </div>
 @endsection
